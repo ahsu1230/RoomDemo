dfab4a95abc13f71dbf4869bac4e14e969df1750
Aaron Hsu <aaron@nextbit.com>
2017-11-07 15:54:10 -0800

    Introduce Stetho

---
+++ app/src/main/java/com/mooseyhsushi/roomdemo/MyApplication.java
@@ -0,0 +1,10 @@
+package com.mooseyhsushi.roomdemo;
+
+import android.app.Application;
+
+public class MyApplication extends Application {
+    public void onCreate() {
+        super.onCreate();
+//        Stetho.initializeWithDefaults(this);
+    }
+}
